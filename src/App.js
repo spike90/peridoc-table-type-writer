d4bfc9d4b9048a93275099c23130f07da645f580
Gabor Racz <raczg90@gmail.com>
2022-05-27 00:33:17 +0300

    optimizing the backtracking

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import data from './data';
 import React from 'react';
 import PrTile from "./PrTile";
 
+const MAX_CHAR = 16;
+
 class App extends React.Component {
     constructor(props) {
         super(props);
@@ -10,40 +12,42 @@ class App extends React.Component {
     }
 
     onChange = event => {
-        if (event.target.value.length < 15) {
-            this.setState({value: event.target.value, tiles: this.calculate(event.target.value.split(' '))});
+        if (event.target.value.length <= MAX_CHAR) {
+            this.setState({value: event.target.value, tiles: this.calculate(event.target.value.split(' '), 0)});
         }
     };
 
-    calculate = (obj) => {
+    calculate = (obj, minElementIndex) => {
         if (obj.filter(item => typeof item === 'string').length === 0) {
             return obj;
         }
 
         let best = [...obj];
-        data.forEach(element => {
-            obj.forEach((item, index) => {
-                if (typeof item === 'string') {
-                    let {abr, id} = element;
-                    abr = abr.toLowerCase();
-                    if (item.toLowerCase().indexOf(abr) > -1) {
-                        let newArr = item.toLowerCase().replace(abr, '#$!' + id + '!#$').split('#$').map(item => {
-                            if (item === '!' + id + '!') {
-                                return id-1;
+        data.forEach((element, elementIndex) => {
+            if (elementIndex >= minElementIndex) {
+                obj.forEach((item, index) => {
+                    if (typeof item === 'string') {
+                        let {abr, id} = element;
+                        abr = abr.toLowerCase();
+                        if (item.toLowerCase().indexOf(abr) > -1) {
+                            let newArr = item.toLowerCase().replace(abr, '#$!' + id + '!#$').split('#$').map(item => {
+                                if (item === '!' + id + '!') {
+                                    return id - 1;
+                                }
+
+                                return item
+                            }).filter(item => item);
+
+                            let newObj = [...obj];
+                            newObj.splice(index, 1, ...newArr);
+                            let probe = this.calculate([...newObj], elementIndex);
+                            if (this.compare(best, probe)) {
+                                best = probe;
                             }
-
-                            return item
-                        }).filter(item => item);
-
-                        let newObj = [...obj];
-                        newObj.splice(index, 1, ...newArr);
-                        let probe = this.calculate([...newObj]);
-                        if (this.compare(best, probe)) {
-                            best = probe;
                         }
                     }
-                }
-            });
+                });
+            }
         });
 
         return best;
@@ -65,7 +69,7 @@ class App extends React.Component {
                     })}
                 </div>
                 <p>
-                    Start typing (max 15 character allowed):
+                    Start typing (max {MAX_CHAR} character allowed):
                 </p>
                 <input type="text" value={this.state.value} onChange={this.onChange}/>
             </div>
